fdbf1d39d0ac72c259def6abb63bae81b7c987bd
jas6zhang <jas6zhang@gmail.com>
2021-03-09 14:57:50 -0500

    completed version

--- src/app.jsx
+++ src/app.jsx
@@ -14,22 +14,68 @@ function App() {
   const [name, setName] = useState('');
   const [id, setId] = useState('');
   const [type, setType] = useState('');
+  const [type2, setType2] = useState('');
   const [sprite, setSprite] = useState('');
+  const [ability, setAbility] = useState('');
+  const [statsHP, setStatsHP] = useState('');
+  const [statsAttack, setStatsAttack] = useState('');
+  const [statsDefense, setStatsDefense] = useState('');
+  const [statsSAttack, setStatsSAttack] = useState('');
+  const [statsSDefense, setStatsSDefense] = useState('');
+  const [statsSpeed, setStatsSpeed] = useState('');
+
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
 
   function search(target) {
     const link = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
     if (target.key === "Enter") {
-      fetch(`http://pokeapi.co/api/v2/pokemon/${query}/`)
+      fetch(`http://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}/`)
         .then(res => res.json())
         .then(result => {
           setPokemon(result);
           setQuery('');
+
+
         if (result) {
           setName(result.species.name);
+          setType('');
           setType(result.types[0].type.name);
+          setType2('');
+          if (result.types[1] !== undefined) {
+            setType2(result.types[1].type.name);
+          }
           setId(result.id);
+          setStatsHP(result.stats[0].base_stat);
+          setStatsAttack(result.stats[1].base_stat);
+          setStatsDefense(result.stats[2].base_stat);
+          setStatsSAttack(result.stats[3].base_stat);
+          setStatsSDefense(result.stats[4].base_stat);
+          setStatsSpeed(result.stats[5].base_stat);
+          setAbility(result.abilities[0].ability.name);
           setSprite(link.concat(result.id, ".png"));
 
+          setHeight(result.height)
+          setWeight (result.weight)
+
+          document.getElementById('line1').classList.remove("progress-line");
+          document.getElementById('line2').classList.remove("progress-line");
+          document.getElementById('line3').classList.remove("progress-line");
+          document.getElementById('line4').classList.remove("progress-line");
+          document.getElementById('line5').classList.remove("progress-line");
+          document.getElementById('line6').classList.remove("progress-line");
+
+          window.setTimeout(addClass, 0)
+
+          function addClass() {
+          document.getElementById('line1').classList.add("progress-line");
+          document.getElementById('line2').classList.add("progress-line");
+          document.getElementById('line3').classList.add("progress-line");
+          document.getElementById('line4').classList.add("progress-line");
+          document.getElementById('line5').classList.add("progress-line");
+          document.getElementById('line6').classList.add("progress-line");
+        }
         }
           console.log(result)
         });
@@ -53,7 +99,9 @@ function App() {
 
 
         <Card
-        pokemonName = {name} type = {type} sprite = {sprite} id = {id}
+        pokemonName = {name} type = {type} type2 = {type2} sprite = {sprite} id = {id} statsHP = {statsHP}
+        statsAttack = {statsAttack} statsDefense = {statsDefense} statsSAttack = {statsSAttack}
+        statsSDefense = {statsSDefense} statsSpeed = {statsSpeed} ability = {ability} height = {height} weight = {weight}
         />
       </div>
       <Footer />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,119 @@
-import React from 'react';
+import React, {useState} from "react";
 import './PokeList';
 import ReactDOM from "react-dom";
-import bug from "./icons/bug.svg";
-import normal from "./icons/normal.svg";
-import fire from "./icons/fire.svg";
-import water from "./icons/water.svg";
-import grass from "./icons/grass.svg";
-import electric from "./icons/electric.svg";
 
 function Card(props) {
 
     const name = props.pokemonName;
     const sprite = props.sprite;
     const type = props.type;
-    const typeimg = type.concat("img");
-    const id = props.id;
+    const type2 = props.type2;
 
+
+    const typeimg = "./icons/".concat(type, ".svg");
+    var type2img = "./icons/".concat(type2, ".svg");
+
+    if (type2 === "") {
+       type2img = "./icons/".concat("blue-1.png");
+
+    }
+
+    const id = props.id;
+    const hp = props.statsHP;
+    const attack = props.statsAttack;
+    const defense = props.statsDefense;
+    const sattack = props.statsSAttack;
+    const sdefense = props.statsSDefense;
+    const speed = props.statsSpeed
+
+    const ability = props.ability;
+
+    const statWidth = "80";
+    const statWidthStyle = {
+      "--content": statWidth
+    }
+
+    const height = props.height;
+    const weight = props.weight;
    return (
     <div className="detail-view">
     <div className ="row">
-      <div className="col-md-4 name">
+      <div className="col-md-6 name">
         <h1 classname = "name">#{id} {name.toUpperCase()}</h1>
-      </div>
-      <div className="col-md-8 stats">
-        <h1>Type: {type}</h1>
-        <div className="icon">
-          <img src ={typeimg}></img>
-          </div>
+        <div className = "picture">
+          <img className = "sprite" src = {sprite}/>
         </div>
       </div>
-    <div className = "picture">
-      <img className = "sprite" src = {sprite}/>
-    </div>
+      <div className="col-md-6 stats">
+        <h1 className = "type">Type(s): {type.charAt(0).toUpperCase() + type.slice(1)} {type2.charAt(0).toUpperCase() + type2.slice(1)}</h1>
+        <div className= {type2.concat(" icon")} id="type2">
+          <img src ={process.env.PUBLIC_URL + type2img}></img>
+        </div>
+        <div className= {type.concat(" icon")}>
+          <img src ={process.env.PUBLIC_URL + typeimg}></img>
+        </div>
+
+        <h1 className = "ability"> Ability: {ability.charAt(0).toUpperCase() + ability.slice(1)}</h1>
+        <h2 className = "ability"> Height: {height}m</h2>
+        <h2 className = "ability"> Weight: {weight / 10}kg</h2>
+        <div className="skill-bars">
+            <div className="bar">
+              <div className="info">
+                <span>HP: {hp}</span>
+              </div>
+              <div className="progress-line hp" id ="line1">
+                <span style={{"width": hp * 1.5}}></span>
+              </div>
+            </div>
+            <div className="bar">
+              <div className="info">
+                <span>ATTACK: {attack}</span>
+              </div>
+              <div className="progress-line attack" id ="line2">
+                <span style={{"width": attack * 1.5}}></span>
+              </div>
+            </div>
+            <div className="bar">
+              <div className="info">
+                <span>DEFENSE: {defense}</span>
+              </div>
+              <div className="progress-line defense" id ="line3">
+
+                <span style={{"width": defense * 1.5}}></span>
+              </div>
+            </div>
+            <div className="bar">
+              <div className="info">
+                <span>SP.ATTACK: {sattack}</span>
+              </div>
+              <div className="progress-line sattack" id ="line4">
+                <span style={{"width": sattack * 1.5}}></span>
+              </div>
+            </div>
+            <div className="bar">
+              <div className="info">
+                <span>SP.DEFENSE: {sdefense}</span>
+              </div>
+              <div className="progress-line sdefense" id ="line5">
+
+                <span style={{"width": sdefense * 1.5}}></span>
+              </div>
+            </div>
+            <div class="bar">
+              <div class="info">
+                <span>SPEED: {speed}</span>
+              </div>
+              <div className="progress-line speed" id ="line6">
+
+                <span style={{"width": speed * 1.5}}></span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
   </div>
   )
 
